bf252d67c1d8467730150c385986e45235329a62
Andrii Sakhno <andysakhno@gmail.com>
2024-03-31 16:38:37 +0300

    Add Github Pages

--- src/components/styles/Themes.js
+++ src/components/styles/Themes.js
@@ -2,8 +2,6 @@ export const theme = {
     main: {
         background: '#ffffff',
         text: '#000000',
-        fadeFrom: '#b23b65',
-        fadeTo: '#fdcc02',
         button: {
             background: '#16FF00',
             color: '#ffffff'
